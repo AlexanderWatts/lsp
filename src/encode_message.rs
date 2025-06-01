7cdbfbf474388a00c7d96c14f9dfe1fee7a5f0b0
Alexander Watts <acwatts.dev@gmail.com>
2025-06-01 14:45:42 +0100

    Updated encode message to accept any value that can be serialized

--- src/encode_message.rs
+++ src/encode_message.rs
@@ -1,11 +1,32 @@
+use serde::Serialize;
 use serde_json::Result;
 
-pub fn encode_message(message: &str) -> Result<String> {
-    let content = serde_json::to_string(message)?;
+pub fn encode_message<T: Serialize>(message: T) -> Result<String> {
+    let content = serde_json::to_string(&message)?;
 
     Ok(format!(
         "Content-Length: {}\r\n\r\n{}",
         content.len(),
         content
     ))
+}
+
+#[cfg(test)]
+mod encode_message {
+    use serde::Serialize;
+
+    use super::*;
+
+    #[test]
+    fn expected() {
+        #[derive(Serialize)]
+        struct Content {
+            id: usize,
+        }
+
+        assert_eq!(
+            "Content-Length: 8\r\n\r\n{\"id\":1}",
+            encode_message(Content { id: 1 }).unwrap()
+        );
+    }
 }
